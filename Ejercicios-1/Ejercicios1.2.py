52fb2a84a117614db88ca848b94900490ab9e71f
said-afk256 <saidantonioburciagaparada@gmail.com>
2026-05-07 11:41:04 -0600

    avance de los ejercicios

---
+++ Ejercicios-1/Ejercicios1.2.py
@@ -0,0 +1,14 @@
+#Porcentaje de material inservible que se reduce en:
+#El promedio de los cursos
+#El curso actual
+
+#Duraciones de los cursos
+
+tiempo_minimo=2.5
+tiempo_maximo=7
+tiempo_este_curso=1.5
+tiempo_promedio=4
+
+crudo_este_curso=3.5
+crudo_otro=5
+
