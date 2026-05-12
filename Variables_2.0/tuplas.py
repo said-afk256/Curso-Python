3dedf5d3dba36fb6294a7f994ec8756d001f42f1
said-afk256 <saidantonioburciagaparada@gmail.com>
2026-05-12 16:39:12 -0600

    Tuplas 2.0

---
+++ Variables_2.0/tuplas.py
@@ -0,0 +1,22 @@
+#12/05/2026
+
+#Función tuple([])
+#Convierte un conjuto de datos a lista
+#lista=["hola","lista"]
+#lista_a_tupla=tuple(lista)#El resultado de la función necesita guardarse en una variable
+#print(lista_a_tupla)
+#print(type(lista_a_tupla))
+#print()
+
+#tupla=tuple(["Dato1","Dato2"]) #Redefinir una tupla
+
+#Creando una tupla sin parentesis de multiples datos
+tupla="Dato1","Dato2"
+
+#Creando una tupla sin parentesis de multiples datos
+tupla="Dato1",
+
+print(type(tupla))
+
+"¿Para que utilizar tuplas? Se usan preferentemente para lectura de datos por ser inmutables"
+#Las tuplas son mejores para datos fijos, las listas para datos flexibles por su facil edición
