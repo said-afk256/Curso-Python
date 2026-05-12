5a29423f5dcad33feedc58e83be5c6bb8974c11f
said-afk256 <saidantonioburciagaparada@gmail.com>
2026-05-12 15:24:43 -0600

    Desempaquetado de variables
    Explicación

---
+++ Variables_2.0/desempaquetado.py
@@ -0,0 +1,25 @@
+#12/05/2026
+#Desempaquetado de variables
+#Es la asignación de variables a un conjunto de datos en matriz de manera sencilla
+
+#Creando una tupla
+datos=("Lucas","Dalto",1000000)
+#Creando una lista
+datos2=["Carlotta","Montelly",17891221]
+
+#Desempaquetado
+nombre,apellido,suscriptores=datos #El desempaquetado de datos solo funciona si se opta por poner 
+#variables a cada uno de los elementos del array 
+#Desempaquetando lista
+nombre1,apellido1,dinero=datos2
+
+#Demostración
+print(nombre)
+print(apellido)
+print(suscriptores)
+print()
+Tupla=datos
+print(Tupla)
+print()
+print(datos2)
+print(dinero)
