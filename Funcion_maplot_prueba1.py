349b35a597a9aef9001ab70752831c6dab65dd62
said-afk256 <saidantonioburciagaparada@gmail.com>
2026-08-10 13:53:51 -0600

    Create Funcion_maplot_prueba1.py

---
+++ Funcion_maplot_prueba1.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt 
+
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 40]
+
+plt.plot(x, y)  # Dibuja un gráfico de líneas
+plt.title("Gráfico de Ejemplo :O")
+plt.show()  # Muestra la ventana con el gráfico
